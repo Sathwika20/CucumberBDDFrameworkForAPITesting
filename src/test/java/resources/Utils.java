e3df073ac918109a9d1977b02b22cab7472797d9
Sathwika20 <gopirajsathwika1999@gmail.com>
2021-09-16 12:25:20 +0530

    Added global.properties file

--- src/test/java/resources/Utils.java
+++ src/test/java/resources/Utils.java
@@ -1,26 +1,29 @@
 package resources;
 
-import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
+import java.io.*;
+import java.util.Properties;
 
 public class Utils {
     RequestSpecification req;
-    public RequestSpecification requestSpecification() throws FileNotFoundException {
+    public RequestSpecification requestSpecification() throws IOException {
         PrintStream log = new PrintStream(new FileOutputStream("logging.txt"));
-        RestAssured.baseURI = "https://rahulshettyacademy.com";
-        req = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com")
+        req = new RequestSpecBuilder().setBaseUri(getGlobalValue("baseURL"))
                 .addQueryParam("key","qalick123")
                 .addFilter(RequestLoggingFilter.logRequestTo(log))
                 .addFilter(ResponseLoggingFilter.logResponseTo(log))
                 .setContentType(ContentType.JSON).build();
         return req;
+    }
+    public String getGlobalValue(String key) throws IOException {
+        Properties properties = new Properties();
+        FileInputStream fis = new FileInputStream("C:\\Users\\gopir\\IdeaProjects\\CucumberBDDFrameworkForAPITesting\\src\\test\\java\\resources\\global.properties");
+        properties.load(fis);
+        return properties.getProperty(key);
     }
 }
